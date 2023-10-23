a8350a8f079b1df3ee16a7e6afb13cb59eeb80a8
Namima1 <129132573+Namima1@users.noreply.github.com>
2023-10-22 23:02:35 -0400

    Add files via upload

---
+++ sample_method.rb
@@ -0,0 +1,12 @@
+#sample method - extract one or more random elements in array 
+flavors = ["Chocolate", "Vanilla", "Strawberry", "Cookies and Cream"]
+
+p flavors.sample 
+
+p flavors.sample(1)
+#this is asking to pull one element 
+ p flavors.sample(2)
+ p flavors.sample(3)
+ p flavors.sample(4)
+
+ #use you use a number larger than the array, it will give you the max amount 
