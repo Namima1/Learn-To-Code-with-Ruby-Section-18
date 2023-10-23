36ffc04216fd157c3d522b7dd0810181209de873
Namima1 <129132573+Namima1@users.noreply.github.com>
2023-10-22 23:03:39 -0400

    Add files via upload

---
+++ array_intersection_with_ampersand_symbol.rb
@@ -0,0 +1,5 @@
+# & symbol - combine multiple arrays, keep elements that are found in both 
+
+p [1, 1, 2, 3, 4, 5] & [1, 4, 5, 8, 9]
+#this combines multiple arrays and only keeps whats in both 
+ p [1, 1, 1, 1, 1, 3, 4, 5, 6] & [3, 7, 55, 56] & [3, 8, 9]
