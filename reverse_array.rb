6362a746e46d4acd9305ea5a6c6488a3b702e561
Namima1 <129132573+Namima1@users.noreply.github.com>
2023-10-22 23:00:40 -0400

    Add files via upload

---
+++ reverse_array.rb
@@ -0,0 +1,11 @@
+#reverse - returns a new array with elements in reverse order 
+
+p [1, 2, 3].reverse 
+p [true, true, false, false].reverse 
+
+#this reverses the original array, it does not alter the original 
+
+queue = [4, 8, 15, 16, 23, 42]
+p queue.reverse 
+p queue 
+#this reverses as well
